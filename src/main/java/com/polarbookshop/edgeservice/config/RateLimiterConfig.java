ae8921da529bde82b16f3c2ce69dbb41ef811467
zzzang12 <wadevs12@gmail.com>
2024-08-16 15:31:32 +0900

    Request rate limiting with Spring Cloud Gateway and Redis

---
+++ src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -0,0 +1,14 @@
+package com.polarbookshop.edgeservice.config;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class RateLimiterConfig {
+	@Bean
+	public KeyResolver keyResolver() {
+		return exchange -> Mono.just("anonymous");
+	}
+}
